3cef22cfe88fbba3146bc61b4fe12190594a146a
hasen2 <huaiyuan67@163.com>
2020-01-08 11:30:08 +0800

    二维码

--- 生成二维码/GetQRcode.py
+++ 生成二维码/GetQRcode.py
@@ -36,7 +36,7 @@ def create_qrcode(url, qrcodename=None, logopic=None, dir=None,style=1):
             version=4, # 设置容错率为最高默认边长是取决于你输入的信息的长度和使用的纠错等级；而默认纠错等级是最高级的H
             level='H', # 控制纠错水平，范围是L、M、Q、H，从左到右依次升高
             picture=logopic, # 用来将QR二维码图像与一张同目录下的图片相结合，产生一张黑白图片
-            colorized=True,  # 可以使产生的图片由黑白(False)变为彩色(True)的
+            colorized=True,  # 可以使产生的图片由黑白(False)变为彩色(True)的(jpg格式的图片不支持彩色）
             contrast=1.0, # 用以调节图片的对比度，1.0 表示原始图片，更小的值表示更低对比度，更大反之。默认为1.0。
             brightness=1.0, # 用来调节图片的亮度，其余用法和取值与 -con 相同
             save_name=qrcodename, # 控制文件名，格式可以是 .jpg， .png ，.bmp ，.gif ；
